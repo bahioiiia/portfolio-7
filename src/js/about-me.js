e72a69fa82de38bc2dc109ddffd8a75dac667286
Zakhar <zaharlazarev123@gmail.com>
2024-07-26 13:50:23 +0300

    swiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -16,8 +16,27 @@ export function swiper1() {
     new Swiper('.swiper-container', {
     navigation: {
       nextEl: '.swiper-button-next',
-
     },
+    slidesPerView: 1,
+  spaceBetween: 0,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 0
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
   });
 })
 }
