fefe46eacae35018471857d5edbf25dbd950a0e1
Lissa2121 <114819609+Lissa2121@users.noreply.github.com>
2024-07-26 21:24:48 +0300

    html-css

---
+++ src/js/reviews.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.reviews-swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+});
