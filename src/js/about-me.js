6885d3eaa1968f8b478fd47319256778912b72b4
Zakhar <zaharlazarev123@gmail.com>
2024-07-27 19:09:29 +0300

    swiper_done

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -12,31 +12,60 @@ export function accordion() {
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle'
 export function swiper1() {
-  document.addEventListener('DOMContentLoaded', function(){
-    new Swiper('.swiper-container', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-    },
-    slidesPerView: 1,
-  spaceBetween: 0,
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 0
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 4,
-      spaceBetween: 40
-    }
+  document.addEventListener('DOMContentLoaded', function() {
+      const swiperSlide = document.querySelector(".swiper-slide")
+      const swiper = new Swiper('.swiper-container', {
+          navigation: {
+              nextEl: '.swiper-button-next',
+          },
+          loop:true,
+          keyboard:{
+            enabled:true,
+          },
+          breakpoints: {
+                  320: {
+              slidesPerView: 2,
+              slidesPerGroup: 1,
+            },
+            768: {
+              slidesPerView: 3,
+              slidesPerGroup: 1,
+            },
+            1440: {
+              slidesPerView: 6,
+              slidesPerGroup: 1,
+            }
+          },
+          on: {
+            slideChange: function() {
+              const slides = document.querySelectorAll('.aboutme-swiper-slide li');
+              slides.forEach(slide => {
+                slide.style.backgroundColor = ''; 
+              });
+              
+              const activeSlide = slides[this.activeIndex];
+              activeSlide.style.backgroundColor = '#ed3b44'; 
+            },
+          },
+      });
+      let currentIndex = 0; 
+      function aboutmeClicker() {
+          const aboutmeButton = document.querySelector(".aboutme-button-1440");
+          const aboutmeSlides = document.querySelectorAll(".aboutme-swiper-slide li");
+          const aboutmeMassive = Array.from(aboutmeSlides); 
+      
+          aboutmeButton.addEventListener("click", handler);
+      
+          function handler() {
+              aboutmeMassive.forEach((slide, index) => {
+                  slide.style.backgroundColor = index === currentIndex ? "#ed3b44" : "transparent";
+              });
+      
+              currentIndex = (currentIndex + 1) % aboutmeMassive.length; 
+          }
+      }
+      
+      aboutmeClicker();
+
+    });
   }
-  });
-})
-}
