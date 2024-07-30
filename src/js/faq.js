a416aea68bd661e7316b1fd73a350e922f66bccb
Polya-Yakushkina <polya.yakushkina@gmail.com>
2024-07-30 21:13:05 +0200

    Commit-9
    icons

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,6 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-// import iconURL from '../img/icons.svg';
+import sprite from '../img/icons.svg'
 
 function createSvgIcon(iconId) {
   return `
@@ -23,20 +23,46 @@ export function faqAccordion() {
     const faqTriggers = document.querySelectorAll('.faq-trigger');
 
     faqTriggers.forEach(trigger => {
-      trigger.insertAdjacentHTML('beforeend', createSvgIcon('closed-accordeon'));
+      trigger.insertAdjacentHTML('beforeend', createSvgIcon('icon-accordeon_svg_mobile-min'));
 
       trigger.addEventListener('click', function () {
         this.classList.toggle('is-active');
         
         const isActive = this.classList.contains('is-active');
-        const iconId = isActive ? 'icon-Vector9' : 'closed-accordeon';
+        const iconId = isActive ? 'icon-accordeon_mobile_opened_svg-min' : 'icon-accordeon_svg_mobile-min';
         this.querySelector('.faq-svg').outerHTML = createSvgIcon(iconId);
-        // const iconUrl = isActive ? `url("${iconArrowUp}")` : `url("${iconArrowDown}")`;
+        // const iconUrl = isActive ? `url("${sprite}#icon-accordeon_mobile_opened_svg-min")` : `url("${sprite}#icon-accordeon_svg_mobile-min")`;
         // this.style.setProperty('--faq-icon-url', iconUrl);
       });
     });
   });
 }
+
+
+
+
+
+// const newAcc = document.querySelector(".faq-accordion-container")
+  
+//   newAcc.addEventListener('click', e => {
+//   const elem = e.target.closest('.faq-trigger');
+//   const aboutMainDiv = elem.closest('.faq-item');
+//   const linkSvgAcc = elem.querySelector('.faq-svg use');
+//   const activeElem = elem.closest('.is-active');
+
+//   if (aboutMainDiv && linkSvgAcc) {
+//     const iconStan = activeElem ? '#icon-accordeon_mobile_opened_svg-min' : '#icon-accordeon_svg_mobile-min';
+//     linkSvgAcc.setAttribute('href', `${Sprite}${iconStan}`);
+//   }
+// }); 
+
+
+
+// // import iconURL from '../img/icons.svg';
+
+
+
+
 
 
 
