67c588dbafd809ba3ae4a95fc016f2562a4aff42
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-07-31 03:25:00 +0300

    CSS+JS Corrected

--- src/js/footer.js
+++ src/js/footer.js
@@ -3,68 +3,133 @@ import axios from "axios";
 const footerModalWindows = document.querySelector(".container-footer-modal")
 const footerModalWindowsHidden = document.querySelector(".hidden")
 const buttonClose = document.querySelector(".button-close");
+const modal = document.querySelector('.modal');
+
+const inputEmail = document.querySelector('.input-email');
+const inputComments = document.querySelector('.input-comments');
 
 
 export function submitForm(event) {
-    event.preventDefault();
-    const form = event.target;
-    
-    const inputEmail = (form.elements.email.value).trim();
-    const inputComments = (form.elements.comments.value).trim();
+  event.preventDefault();
+  const form = event.target;
 
-    if (inputEmail === "" || inputComments === "") {
-        alert(`All form fields must be filled in`)
-    } else {
-        const data = {
-            email: `${inputEmail}`,
-            comments: `${inputComments}`
-        }
-        console.log("data", data);  //  перевірка введеного об'єкту
-        form.reset();  //  очищення форми
-        apiCreate({ data })
-            .then((resul) => {
-                console.log("відповідь +");
-                console.log("resul", resul)
-            })
-            .catch((err) => console.log("err", err))
-        
-    loadingTextRemove();
-    }
+  const email = form.elements.email.value.trim();
+  const comment = form.elements.comments.value.trim();
+
+  if (email === '' && comment === '') {
+    colorAddRedEmail();
+    colorAddRedComment();
+    alert(`All form fields must be filled in`);
+    return;
+  } else if (email === '' || !comment === '') {
+    colorAddRedEmail();
+    colorAddGreenComment();
+    alert(`Email form fields must be filled in`);
+    return;
+  } else if (comment === '' || !email === '') {
+    colorAddRedComment();
+    colorAddGreenEmail();
+    alert(`Comment form fields must be filled in`);
+    return;
+  } else {
+    colorRemoveRedEmail();
+    colorRemoveRedComment();
+    colorAddGreenEmail();
+    colorAddGreenComment();
+    const data = {
+      email,
+      comment,
+    };
+    // console.log(data); //  перевірка введеного об'єкту
+    PostnewData(data);
+  }
+  form.reset(); //  очищення форми
 }
 
 
-const URL_API = "https://portfolio-js.b.goit.study/api"
+function PostnewData(data) {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  };
 
-
-function apiCreate(data) {
-    const options = {
-            method: "POST",
-            body: JSON.stringify(data),
-            heder: {
-                "Content-Tape":"application/json"
-                    }
-            }
-    return fetch(`${URL_API}/requests`, options)
-        .then((res) => {
-            if (!res.ok) {
-            throw new Error(res.status)
-            }
-            return res.json
+  fetch('https://portfolio-js.b.goit.study/api/requests', options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then((post) => {
+      const arr = [post];
+      console.log('post', arr);
+      const marcup = arr.map(title => {
+          return `<li>
+            <h3 class="modal-footer-title">${title.title}</h3>
+          </li>
+          <li>
+            <p class="modal-footer-text">${title.message}</p>
+          </li>
+        `;
         })
-    
+        .join('');
+      modal.insertAdjacentHTML('beforeend', marcup);
+    })
+    .catch(error => console.log(error));
+  modal.innerHTML = '';
+  loadingTextRemove();
 }
 
 
+//  активація та приберання модельного вікна
 buttonClose.addEventListener(`click`, loadingTextAdd);
 footerModalWindowsHidden.addEventListener(`click`, loadingTextAdd)
 
-
-
-function loadingTextRemove(){   //  ф-ція додавання footerModalWindows
-    footerModalWindows.classList.remove("hidden")
+function loadingTextRemove(){   //  ф-ція активації footerModalWindows
+  footerModalWindows.classList.remove("hidden")
+  colorRemoveGreenEmail();
+  colorRemoveGreenComment();
 }
 
 function loadingTextAdd(){      //  ф-ція приховування footerModalWindows
-    footerModalWindows.classList.add("hidden")
+  footerModalWindows.classList.add("hidden");
+  
+}
+
+// ------  ф-ції підкреслення error input -----
+
+function colorAddGreenEmail() {  //  ф-ція додавання class green inputEmail
+  inputEmail.classList.add('green');
+}
+
+function colorAddGreenComment() {  //  ф-ція додавання class green inputComments
+  inputComments.classList.add('green');
+}
+
+function colorRemoveGreenEmail() {  //  ф-ція видалення class green inputEmail
+  inputEmail.classList.remove('green');
+}
+
+function colorRemoveGreenComment() {  //  ф-ція видалення class green inputComments
+  inputComments.classList.remove('green');
+}
+
+function colorAddRedEmail() {  //  ф-ція додавання class red inputEmail
+  inputEmail.classList.add('red');
+}
+
+function colorAddRedComment() {  //  ф-ція додавання class red inputComments
+  inputComments.classList.add('red');
+}
+
+function colorRemoveRedEmail() {  //  ф-ція видалення class red inputEmail
+  inputEmail.classList.remove('red');
+}
+
+function colorRemoveRedComment() {  //  ф-ція видалення class red inputComments
+  inputComments.classList.remove('red');
 }
 
